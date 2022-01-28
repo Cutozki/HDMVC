d51a8b8515be365aab148534a5e7fffc195189aa
MM MOVIES <98547195+MMMOVIESS@users.noreply.github.com>
2022-01-28 13:27:30 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -67,6 +67,21 @@ Need to delete lots of messages? That's what purges are for!
 -> <code>/purgefrom</code>
 - Mark the message to purge to (as a reply). All messages between the previously marked /purgefrom and the newly marked /purgeto will be deleted.
 -> /purgeto"""
+    LOKE_TXT = """<b>Locks</b>
+
+Do stickers annoy you? or want to avoid people sharing links? or pictures? You're in the right place!
+
+The locks module allows you to lock away some common items in the telegram world; the bot will automatically delete them!
+
+<b>Admin commands:</b>
+- /lock <item(s)>: Lock one or more items. Now, only admins can use this type!
+- /unlock <item(s)>: Unlock one or more items. Everyone can use this type again!
+- /locks: List currently locked items.
+- /lockwarns <yes/no/on/off>: Enabled or disable whether a user should be warned when using a locked item.
+- /locktypes: Show the list of all lockable items.
+- /allowlist <url/id/@channelname(s)>: Allowlist a URL, group ID, channel @, or bot @ to stop them being deleted by URL, forward, invitelink, and inline locks. Separate with a space to add multiple items at once. If no arguments are given, returns the current allowlist.
+- /rmallowlist <url/id/@channelname(s)>: Remove an item from the allowlist - url, invitelink, and forward locking will now take effect on messages containing it again. Separate with a space to remove multiple items.
+- /rmallowlistall: Remove all allowisted items."""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
 - This BoT Supports both url and alert inline buttons.
