cbddcd9c526bb7703976ed181c768d122f64ca56
MM MOVIES <98547195+MMMOVIESS@users.noreply.github.com>
2022-01-29 23:58:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,7 +406,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='BQADBQADjAMAArkjSFT_BIHEYYfcqRYE'),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='ഒന്ന് പോയേടാ ചെക്കാ...😂'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/IET_UPDATES')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
