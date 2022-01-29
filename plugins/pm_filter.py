b62d20866b0e0a62e9bc0cd51de14ff7b86ca568
MM MOVIES <98547195+MMMOVIESS@users.noreply.github.com>
2022-01-29 09:38:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -933,7 +933,7 @@ async def advantage_spell_chok(msg):
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(100)
+        await asyncio.sleep(16)
         await k.delete()
 
 
