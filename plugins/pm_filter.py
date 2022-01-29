85b5189511e08c4ce2953002fc813c0ce281946e
MM MOVIES <98547195+MMMOVIESS@users.noreply.github.com>
2022-01-29 09:56:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -924,7 +924,12 @@ async def advantage_spell_chok(msg):
         await k.delete()
         return
     SPELL_CHECK[msg.message_id] = movielist
-    btn = [InlineKeyboardButton('🤖 Updates', url='https://t.me/IET_UPDATES')]
+    btn = [[
+        InlineKeyboardButton(
+            text=movie.strip(),
+            callback_data=f"spolling#{user}#{k}",
+        )
+    ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.reply("I couldn't find anything related to that\nDid you mean any one of these.?",
                     reply_markup=InlineKeyboardMarkup(btn))
